b51d0b343efd34c66a29346cd198a8bbf2c8871f
cognac <femt456@gmail.com>
2018-06-23 08:13:08 +0000

    kadai_1

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_secure_password
   
   has_many :posts #,dependent: :destroy
-  has_many :likes #,dependent: :destroy
+  has_many :likes ,dependent: :destroy
   has_many :fav_posts, through: :likes, source: :post #,dependent: :destroy
   
   def favo(post)
